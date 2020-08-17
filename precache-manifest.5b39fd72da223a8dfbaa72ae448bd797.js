a1c3b1b0269535dfd5c272690652e68e4b1e30be
Maarit Laakso <maarit.laakso2@myy.haaga-helia.fi>
2020-08-17 11:23:48 +0300

    Updates

---
+++ precache-manifest.5b39fd72da223a8dfbaa72ae448bd797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40cfa71b8e0591074ab4f1f4cd49697c",
+    "url": "/pers0nal_trainer/index.html"
+  },
+  {
+    "revision": "45f181025f727333e256",
+    "url": "/pers0nal_trainer/static/css/2.d694ff75.chunk.css"
+  },
+  {
+    "revision": "906e563eb9980da205c6",
+    "url": "/pers0nal_trainer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45f181025f727333e256",
+    "url": "/pers0nal_trainer/static/js/2.c27091e0.chunk.js"
+  },
+  {
+    "revision": "5f85ca31f256ef591e7c4aa4ef33123a",
+    "url": "/pers0nal_trainer/static/js/2.c27091e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906e563eb9980da205c6",
+    "url": "/pers0nal_trainer/static/js/main.3cdc36ad.chunk.js"
+  },
+  {
+    "revision": "41035ca00b6e7385723a",
+    "url": "/pers0nal_trainer/static/js/runtime-main.ab739cc8.js"
+  }
+]);
